f133d338b2261e72be9f28c53ed42301acce47b5
Pooja Singh <poojasingh31280@gmail.com>
2024-05-25 16:57:40 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db.js');
+const User = require('./models/user');
 
 const app = express();
 
@@ -13,6 +14,20 @@ app.use(express.json());
 
 // Define routes
 app.get('/', (req, res) => res.send('API running'));
+
+
+app.post('/users', async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+      let user = new User({ name, email, password });
+      await user.save();
+      res.send('User saved');
+      console.log(user)
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
